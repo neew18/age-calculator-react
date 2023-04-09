5fe7700802517374bacd27e938ef44ca9acf6f1b
Nann Ei Ei Win <nanneieiwinn37@gmail.com>
2023-04-09 17:57:30 +0630

    Fix input tags and install react-hook-form
    Rewrite input fields to use with react hook form validation. Installed the package and test it.

--- age-calculator/src/SearchEngine.jsx
+++ age-calculator/src/SearchEngine.jsx
@@ -1,16 +1,38 @@
 import React from 'react'
-import Input from './Input'
+import {useForm} from "react-hook-form"
 
-const labels = ["day","month","year"]
 function SearchEngine() {
+  const { register, handleSubmit, watch, formState: { errors } } = useForm({mode:"all"});
+  const onSubmit = data => console.log(data);
+
+  console.log(watch()); // watch input value by passing the name of it
   return (
     <div className='SearchEngine'>
-        <form>
-            <div className=' flex justify-between'>
-                {labels.map((label)=>{
-                    return <Input label={label}/>
-                })}
+        <form onSubmit={handleSubmit((data)=>{
+            console.log(data);
+        })}> {/*will validate the inputs before invoking "onSubmit" */}
+            <div className='flex justify-between'>
+                <div>
+                    <label className='block text-[#716f6f] font-bold uppercase tracking-widest text-[0.6rem] my-1' >
+                        day
+                    </label>
+                    <input className='border w-24 rounded-md' {...register("day",{ required: "can't be blank" })}/>
+                </div>
+                <div>
+                    <label className='block text-[#716f6f] font-bold uppercase tracking-widest text-[0.6rem] my-1' >
+                        month
+                    </label>
+                    <input className='border w-24 rounded-md' {...register("month",{ required: "can't be blank" })}/>
+                </div>
+                <div>
+                    <label className='block text-[#716f6f] font-bold uppercase tracking-widest text-[0.6rem] my-1' >
+                        year
+                    </label>
+                    <input className='border w-24 rounded-md' {...register("year",{ required: "can't be blank" })} />
+                </div>
             </div>
+            
+            
         </form>
     </div>
   )
